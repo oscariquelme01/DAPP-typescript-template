d1cc228f6e28cfed57127b4d3a9896033e0d40f7
oscariquelme01 <oscariquelme01@gmail.com>
2022-09-20 10:40:23 +0200

    add nodemon to the template

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { example } from "./modules/example"
+
+new example("Hello world!!")
+
+
---
+++ src/modules/example.ts
@@ -0,0 +1,5 @@
+export class example {
+    constructor(s: string) {
+        console.log(s)
+    }
+}
